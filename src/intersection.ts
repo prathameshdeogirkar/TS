590d38368f99a17c5e5d869c00ba8f7f65669f1f
Prathamesh Deogirkar <prathameshdeogirkar22485@gmail.com>
2024-11-27 12:24:17 +0530

    inersection

---
+++ src/intersection.ts
@@ -0,0 +1,22 @@
+interface Student {
+    name: string;
+    age: number;
+    mobile: string;
+}
+
+interface Teacher {
+    name: string;
+    age: number;
+    email: string;
+}
+
+
+type Principal = Student & Teacher;
+
+
+const principal: Principal = {
+    name: Rahul,
+    age: 30,
+    mobile: "",
+    email: "",
+} 
